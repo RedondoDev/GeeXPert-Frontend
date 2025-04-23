17d321bf302863a13c43e3c5827eb2892ccbb6be
RedondoDEV <javierRedondoDEV@gmail.com>
2025-04-23 23:46:38 +0200

    pre-trending-games styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import {SigninComponent} from './auth/signin/signin.component';
 import {SignupComponent} from './auth/signup/signup.component';
 import {HomeComponent} from './content/home/home.component';
-import {ErrorComponent} from './error/error/error.component';
+import {ErrorComponent} from './error/error.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
--- src/app/content/trending/trending.component.ts
+++ src/app/content/trending/trending.component.ts
@@ -1,11 +1,116 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {NgForOf, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-trending',
-  imports: [],
+  imports: [
+    NgForOf,
+    NgIf
+  ],
   templateUrl: './trending.component.html',
   styleUrl: './trending.component.css'
 })
 export class TrendingComponent {
+
+  games = [
+    {
+      id: 1,
+      name: 'Game 1',
+      cover: '//images.igdb.com/igdb/image/upload/t_cover_big/co1wz4.jpg',
+      genres: ['Action', 'Adventure'],
+      platforms: ['PC', 'PS5', 'Xbox', 'PSP', 'PSVita', 'Switch'],
+      rating: 4.5,
+      release_date: '2023-01-15'
+    },
+    {
+      id: 2,
+      name: 'Game 2',
+      cover: '//images.igdb.com/igdb/image/upload/t_cover_big/co1wz4.jpg',
+      genres: ['Terror', 'Adventure'],
+      platforms: ['PS2', 'PSP'],
+      rating: 3,
+      release_date: '2022-11-10'
+    },
+    {
+      id: 3,
+      name: 'Game 3',
+      cover: '//images.igdb.com/igdb/image/upload/t_cover_big/co1wz4.jpg',
+      genres: ['Action', 'Shooter'],
+      platforms: ['PC', 'Xbox'],
+      rating: 4.2,
+      release_date: '2021-06-20'
+    },
+    {
+      id: 4,
+      name: 'Game 4',
+      cover: '//images.igdb.com/igdb/image/upload/t_cover_big/co1wz4.jpg',
+      genres: ['RPG', 'Adventure'],
+      platforms: ['PC', 'PS5'],
+      rating: 4.8,
+      release_date: '2020-09-05'
+    },
+    {
+      id: 5,
+      name: 'Game 5',
+      cover: '//images.igdb.com/igdb/image/upload/t_cover_big/co1wz4.jpg',
+      genres: ['Puzzle', 'Adventure'],
+      platforms: ['PC', 'Switch'],
+      rating: 3.9,
+      release_date: '2019-03-12'
+    },
+    {
+      id: 6,
+      name: 'Game 6',
+      cover: '//images.igdb.com/igdb/image/upload/t_cover_big/co1wz4.jpg',
+      genres: ['Puzzle', 'Adventure'],
+      platforms: ['PC', 'Switch'],
+      rating: 3.9,
+      release_date: '2018-07-22'
+    },
+    {
+      id: 7,
+      name: 'Game 7',
+      cover: '//images.igdb.com/igdb/image/upload/t_cover_big/co1wz4.jpg',
+      genres: ['Action', 'Adventure'],
+      platforms: ['PC', 'PS5'],
+      rating: 4.5,
+      release_date: '2017-05-30'
+    },
+    {
+      id: 8,
+      name: 'Game 8',
+      cover: '//images.igdb.com/igdb/image/upload/t_cover_big/co1wz4.jpg',
+      genres: ['Terror', 'Adventure'],
+      platforms: ['PS2', 'PSP'],
+      rating: 3,
+      release_date: '2016-12-18'
+    },
+    {
+      id: 9,
+      name: 'Game 9',
+      cover: '//images.igdb.com/igdb/image/upload/t_cover_big/co1wz4.jpg',
+      genres: ['Action', 'Shooter'],
+      platforms: ['PC', 'Xbox'],
+      rating: 4.2,
+      release_date: '2015-08-14'
+    },
+    {
+      id: 10,
+      name: 'Game 10',
+      cover: '//images.igdb.com/igdb/image/upload/t_cover_big/co1wz4.jpg',
+      genres: ['RPG', 'Adventure'],
+      platforms: ['PC', 'PS5'],
+      rating: 4.8,
+      release_date: '2014-02-25'
+    }
+  ];
+
+  isInCollection(gameId: number) {
+    return gameId % 2 === 0;
+  }
+
+  addQuitToCollection(gameId: number) {
+    gameId++;
+  }
 
 }
